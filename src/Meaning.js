96f0b636198d9d68f743b6b8b25a443f734fc8db
Aji Sainabou Njie <njieajisai@gmail.com>
2023-01-05 00:32:07 -0500

    updated the synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,21 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partofspeech}</h3>
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
-              <br /> <em>{definition.example}</em>
+              <br />
+              <strong>Example:</strong> <em>{definition.example}</em>
             </p>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
