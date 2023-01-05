baaeb456cec1862e2071efbd78b350e49ca1c41c
Aji Sainabou Njie <njieajisai@gmail.com>
2023-01-04 22:11:07 -0500

    added dictionary API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    return response.date[0];
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} defination`);
   }
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
